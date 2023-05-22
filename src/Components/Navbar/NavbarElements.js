587a17a6840e486dc20595bb38b2bdbc8aaa1a6f
abdallahjabr47 <{ID}+{username}@users.noreply.github.com>
2023-05-22 21:14:01 +0300

    The navbar has been styled, and it's ready to view

---
+++ src/Components/Navbar/NavbarElements.js
@@ -0,0 +1,6 @@
+import AppBar from '@mui/material/AppBar';
+import { styled } from '@mui/system';
+
+export const StyledAppBar = styled(AppBar)`
+  background: linear-gradient(rgb(91, 87, 115), rgb(91, 87, 115));
+`;
--- src/Layout/HomeLayout/HomeLayout.js
+++ src/Layout/HomeLayout/HomeLayout.js
@@ -5,16 +5,19 @@ import Footer from '../../Components/Footer/Footer'
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import './HomeLayout.css';
 import AboutUs from "../../Components/AboutUs";
+import ResponsiveAppBar from "../../Components/Navbar/index";
 
 function HomeLayout() {
   const [activeTab, setActiveTab] = useState(routeNames.HOME);
 
   return (
     <div className="homeStyle">
+
       <Sidebar activeTab={activeTab} />
 
       <div className="pageStyle">
         <div className="outletStyle">
+          <ResponsiveAppBar />
           <Outlet context={setActiveTab} />
         </div>
 
