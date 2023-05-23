d993465837a698d0ac9409eb15ecc43999ca6a38
abdallahjabr47 <{ID}+{username}@users.noreply.github.com>
2023-05-23 19:22:30 +0300

    add 2 cards

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
-import { useEffect, Suspense } from 'react';
+import { Suspense } from 'react';
 import './App.css';
-import { getTokenFromUrl } from './Logic/spotify';
-import SpotifyWebApi from 'spotify-web-api-js';
-import {useDataLayerValue} from './Logic/DataLayer';
 import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import { routes } from './Utils/Utils';
 import HomeLayout from './Layout/HomeLayout/HomeLayout';
 import NotFound from './Pages/NotFound/NotFound';
 import Login from './Components/Login/Login';
 
-const spotify = new SpotifyWebApi();
 
 function App() {
-  //useEffect here used with variables to show and save the value on a state to render the next page.
-  //to grap something from datalayer
-  const [{ user , token }, dispatch] = useDataLayerValue();
 
-  //useEffect here used to run the code based on given condition
-  useEffect(() => {
-    const hash = getTokenFromUrl();
-    window.location.hash = "";
-    const tempToken = hash.access_token;
-
-    if(tempToken) {
-      dispatch({
-        type: "SET_TOKEN",
-        token: tempToken
-      });
-
-      //connect spotify to React
-      //Put a key in API
-      spotify.setAccessToken(tempToken);
-      //Get the user account
-      spotify.getMe().then(user => {        
-        dispatch({
-          type: 'SET_USER',
-          user: user
-        });
-      });
-
-      spotify.getUserPlaylists()
-      .then((playlists) => {
-        dispatch({
-          type: "SET_PLAYLISTS",
-          playlists: playlists,
-        });
-      });
-
-      // spotify.getPlaylists("id")
-      // .then(response => {
-      //   dispatch({
-      //     type: "SET_DISCOVER_WEEKLY",
-      //     discover_weekly: response,
-      //   });
-      // });
-
-    }
-  }, []);
 
   //console.log("Me", user);
   //console.log("token", token);
--- src/Layout/HomeLayout/HomeLayout.js
+++ src/Layout/HomeLayout/HomeLayout.js
@@ -1,16 +1,67 @@
-import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState , useEffect} from "react";
+import { Outlet , useLocation } from "react-router-dom";
 import { routeNames } from "../../Utils/Utils";
 import Footer from '../../Components/Footer/Footer'
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import './HomeLayout.css';
 import AboutUs from "../../Components/AboutUs";
 import ResponsiveAppBar from "../../Components/Navbar/Navbar";
+import { getTokenFromUrl ,spotify } from '../../Logic/spotify';
+import {useDataLayerValue} from '../../Logic/DataLayer';
 
-function HomeLayout({ spotify }) {
+function HomeLayout() {
   const [activeTab, setActiveTab] = useState(routeNames.HOME);
 
+  //useEffect here used with variables to show and save the value on a state to render the next page.
+  //to grap something from datalayer
+  const [{ user , token }, dispatch] = useDataLayerValue();
+  const location = useLocation();
+
+  //useEffect here used to run the code based on given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl( location.hash );
+    location.hash = "";
+    const tempToken = hash.access_token;
+
+    if(tempToken) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: tempToken
+      });
+
+      //connect spotify to React
+      //Put a key in API
+      spotify.setAccessToken(tempToken);
+      localStorage.setItem("token", tempToken);
+      //Get the user account
+      spotify.getMe().then(user => {        
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
+      });
+
+      spotify.getUserPlaylists()
+      .then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        });
+      });
+
+      // spotify.getPlaylists("id")
+      // .then(response => {
+      //   dispatch({
+      //     type: "SET_DISCOVER_WEEKLY",
+      //     discover_weekly: response,
+      //   });
+      // });
+
+    }
+  }, []);
+
   return (
+    
     <div className="homeStyle">
 
       <Sidebar activeTab={activeTab} />
