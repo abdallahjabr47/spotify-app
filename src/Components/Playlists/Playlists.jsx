b3e3debc60bce34ba7a7c016487f9e7602e49019
abdallahjabr47 <{ID}+{username}@users.noreply.github.com>
2023-06-07 00:52:38 +0300

    Added active tab feature to the playlists in the sidebar

--- src/Components/Playlists/Playlists.jsx
+++ src/Components/Playlists/Playlists.jsx
@@ -1,23 +1,21 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
 import { reducerCases } from "../../Logic/Constants";
 import { useDataLayerValue } from "../../Logic/DataLayer";
 import { Container } from "@mui/material";
 import { spotify } from "../../Logic/spotify";
-import './PlaylistStyle.css'
-import { useNavigate  } from "react-router-dom"; // Import useHistory
-
+import "./PlaylistStyle.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Playlists() {
-  const navigate = useNavigate(); // Get the navigate function
+  const navigate = useNavigate();
   const [{ token, playlists }, dispatch] = useDataLayerValue();
-  
+  const [selectedPlaylistId, setSelectedPlaylistId] = useState(null); // Define selectedPlaylistId state variable
+
   useEffect(() => {
     const getPlaylistData = async () => {
       try {
-        let data = await spotify.getUserPlaylists(undefined, {offset: 5, limit: 10})
-        
+        let data = await spotify.getUserPlaylists(undefined, { offset: 5, limit: 10 });
+
         const { items } = data;
         const playlists = items.map(({ name, id }) => {
           return { name, id };
@@ -35,15 +33,17 @@ export default function Playlists() {
 
   const changeCurrentPlaylist = (selectedPlaylistId, name) => {
     dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
-    navigate("/", {state: {page: name}}); // Navigate to the home page ("/")
+    setSelectedPlaylistId(selectedPlaylistId); 
+    navigate("/", { state: { page: name } });
   };
-  
+
   return (
     <Container>
       <ul className="ulStyle">
         {playlists.map(({ name, id }) => {
+          const listItemClasses = `liStyle ${selectedPlaylistId === id ? "active" : ""}`;
           return (
-            <li className="liStyle" key={id} onClick={() => changeCurrentPlaylist(id, name)}>
+            <li className={listItemClasses} key={id} onClick={() => changeCurrentPlaylist(id, name)}>
               {name}
             </li>
           );
@@ -51,4 +51,4 @@ export default function Playlists() {
       </ul>
     </Container>
   );
-};
+}
